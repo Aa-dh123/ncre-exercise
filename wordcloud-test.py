daa337a205cf09f908a7bf6e85ba8a03a0e7829f
Aa-dh123 <33516711+Aa-dh123@users.noreply.github.com>
2018-09-15 19:33:14 +0800

    Update wordcloud-test.py

--- wordcloud-test.py
+++ wordcloud-test.py
@@ -2,10 +2,10 @@ import jieba
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
-background_imag = plt.imread('51452-106.jpg')
+#background_imag = plt.imread('51452-106.jpg')
 f = open('大唐万户侯.txt','r',encoding = 'utf-8')
 txt = f.read()
 words = jieba.lcut(txt)
 newtxt = ''.join(words)
-wordcloud = WordCloud(font_path='C:\\Windows\\Fonts\\simkai.ttf',mask=background_imag).generate(newtxt)
+wordcloud = WordCloud(font_path='C:\\Windows\\Fonts\\simkai.ttf').generate(newtxt)
 wordcloud.to_file('词云效果图.png')
